5e5b49101bd17300c090d57604ded523a8438e4e
bkkim2897 <olivia1841@naver.com>
2020-08-09 02:12:28 +0900

    Add class, placeholder

--- JssProject/main/forms.py
+++ JssProject/main/forms.py
@@ -6,3 +6,18 @@ class JssForm(forms.ModelForm):
     class Meta:
         model = Jasoseol
         fields = ('title', 'content',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__( *args, **kwargs)
+
+        self.fields['title'].label = '제목'
+        self.fields['content'].label = '자기소개서 내용'
+
+        self.fields['title'].widget.attrs.update({
+            'class' : 'jss_title',
+            'placeholder' : '제목'
+        })
+
+        self.fields['content'].widget.attrs.update({
+            'placeholder' : '내용을 입력하세요'
+        })
