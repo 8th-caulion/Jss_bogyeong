dd7411a222648a5759838fd6c65829032055efec
bkkim2897 <olivia1841@naver.com>
2020-08-10 02:09:41 +0900

    create Comment

--- JssProject/main/forms.py
+++ JssProject/main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Jasoseol
+from .models import Jasoseol, Comment
 
 class JssForm(forms.ModelForm):
 
@@ -21,3 +21,9 @@ class JssForm(forms.ModelForm):
         self.fields['content'].widget.attrs.update({
             'placeholder' : '내용을 입력하세요'
         })
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('content', )
--- JssProject/main/models.py
+++ JssProject/main/models.py
@@ -8,3 +8,8 @@ class Jasoseol(models.Model):
     content = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE) #USER가 지워지면 그에 해당하는 자소서도 지워짐
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #USER가 지워지면 그에 해당하는 자소서도 지워짐
+    jasoseol = models.ForeignKey(Jasoseol, on_delete=models.CASCADE)
